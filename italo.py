91cf8a33c8516ca5fe9596ed5dc61478abde994b
italonogueira <147431079+italonogxz@users.noreply.github.com>
2023-10-23 13:52:55 -0300

    Add files via upload

---
+++ italo.py
@@ -0,0 +1,7 @@
+# Digite uma metragem e veja isso em cm e mm:
+
+metragem = float(input("digite sua metragem"))
+centimetros = metragem*100
+milimetros = metragem*1000
+print("número em cm:", centimetros,)
+print("número em mm:", milimetros)
